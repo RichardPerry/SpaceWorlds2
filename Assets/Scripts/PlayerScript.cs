b810056bbc707dd06934d0ba575d788db706caf3
David Truby <david.truby@gmail.com>
2014-10-22 16:24:23 +0100

    Fixed center of rotation and camera.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,32 +3,34 @@
 public class PlayerScript : MonoBehaviour
 {
 
-	public const int speed = 10;
-	private float angle = 0.0f;
-	private float dangle = 0.0f;
-	private float throttle = 0;
+    public const int speed = 10;
+    private float angle = 0.0f;
+    private float dangle = 0.0f;
+    private float throttle = 0;
 
-	// Use this for initialization
-	void Start() {
-	
-	}
-	
-	// Update is called once per frame
-	void Update() {
-		var inputX = Input.GetAxis("Horizontal");
-		var inputY = Input.GetAxis("Vertical");
+    // Use this for initialization
+    void Start()
+    {
 
-		throttle = speed * inputY;
-		dangle = inputX * -2;
-	}
+    }
 
-	void FixedUpdate()
-	{
-		transform.Rotate(0,0,dangle);
-		transform.FindChild("Main Camera").gameObject.camera.transform.Rotate(0,0,-dangle);
-		angle += dangle;
-		var x = throttle * -Mathf.Sin(angle * Mathf.Deg2Rad);
-		var y = throttle * Mathf.Cos(angle * Mathf.Deg2Rad);
-		rigidbody2D.velocity = new Vector2(x,y);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        var inputX = Input.GetAxis("Horizontal");
+        var inputY = Input.GetAxis("Vertical");
+
+        throttle = speed * inputY;
+        dangle = inputX * -2;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0,0,dangle);
+        transform.FindChild("Main Camera").gameObject.camera.transform.Rotate(0,0,-dangle);
+        angle += dangle;
+        var x = throttle * -Mathf.Sin(angle * Mathf.Deg2Rad);
+        var y = throttle * Mathf.Cos(angle * Mathf.Deg2Rad);
+        rigidbody2D.velocity = new Vector2(x,y);
+    }
 }
